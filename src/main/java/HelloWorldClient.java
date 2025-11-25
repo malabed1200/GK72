ee7b15ec18bc3597f102ca90b6f9183d884e1fc6
malabed1200 <malabed@student.tgm.ac.at>
2025-11-25 15:50:22 +0100

    Add files via upload

---
+++ src/main/java/HelloWorldClient.java
@@ -0,0 +1,26 @@
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class HelloWorldClient {
+
+    public static void main(String[] args) {
+
+        String firstname = args.length > 0 ? args[0] : "Max";
+        String lastname  = args.length > 1 ? args[1] : "Mustermann";
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+                .usePlaintext()
+                .build();
+
+        HelloWorldServiceGrpc.HelloWorldServiceBlockingStub stub = HelloWorldServiceGrpc.newBlockingStub(channel);
+
+        Hello.HelloResponse helloResponse = stub.hello(Hello.HelloRequest.newBuilder()
+                .setFirstname(firstname)
+                .setLastname(lastname)
+                .build());
+        System.out.println( helloResponse.getText() );
+        channel.shutdown();
+
+    }
+
+}
